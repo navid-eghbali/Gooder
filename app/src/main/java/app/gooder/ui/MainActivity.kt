381f97c8b13cf7e340468bdece94ee13b3c6de1e
Navid Eghbali <navideghbali@gmail.com>
2023-01-03 21:03:14 +0100

    Adjust dark themes

--- app/src/main/java/app/gooder/ui/MainActivity.kt
+++ app/src/main/java/app/gooder/ui/MainActivity.kt
@@ -3,14 +3,18 @@ package app.gooder.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import gooder.core.ui.compose.DevicePreviews
 import gooder.core.ui.designsystem.GooderTheme
 
@@ -18,10 +22,27 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             GooderTheme {
-                Greeting()
+                Home()
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Home() {
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Greeting()
         }
     }
 }
@@ -43,6 +64,6 @@ fun Greeting() {
 @Composable
 fun DefaultPreview() {
     GooderTheme {
-        Greeting()
+        Home()
     }
 }
